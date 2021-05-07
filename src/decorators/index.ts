0a30eaa086f3e57849e6f08aedb60bbf225256a3
Jose Miguel <jose@agenciampd.com>
2021-05-07 16:23:06 +0200

    Inject eventbus exported

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,5 +1,6 @@
 export * from './command-handler.decorator';
 export * from './events-handler.decorator';
 export * from './inject-command-bus.decorator';
+export * from './inject-event-bus.decorator';
 export * from './inject-query-bus.decorator';
 export * from './query-handler.decorator';
